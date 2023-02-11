af052a902ec3a8b7c279cdbd4af392be24916b8f
Jordan Butterwick <jordan@butterwick.tech>
2023-02-11 00:47:32 -0700

    feat: client adds plants to world and announces them

--- client/main.rs
+++ client/main.rs
@@ -1,3 +1,43 @@
+use bevy::prelude::*;
+use bevy::app::App;
+
+#[derive(Component)]
+struct Position { x: f32, y: f32 }
+
+#[derive(Component)]
+struct Name(String);
+
+#[derive(Component)]
+struct Plant;
+
+struct Entity(u64);
+
+fn print_position_system(query: Query<&Position>) {
+    for position in query.iter() {
+        println!("x: {:?}, y: {:?}", position.x, position.y);
+    }
+}
+
+fn announce_plants(query: Query<&Name, With<Plant>>) {
+    for name in query.iter() {
+        println!("hello {}!", name.0);
+    }
+}
+
+fn hello_world() {
+    println!("hello world!");
+}
+fn add_plants(mut commands: Commands) {
+    commands.spawn((Plant, Name("Carrot".to_string())));
+    commands.spawn((Plant, Name("Potato".to_string())));
+    commands.spawn((Plant, Name("Tomato".to_string())));
+}
+
 fn main() {
-    println!("Hello, world!");
+    App::new()
+        .add_startup_system(add_plants)
+        .add_system(hello_world)
+        .add_system(print_position_system)
+        .add_system(announce_plants)
+        .run();
 }
