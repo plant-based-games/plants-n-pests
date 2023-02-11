0ec34390dba1088853ccba1dc2a4efdbf4c079a5
Lyall Jonathan Di Trapani <lj.ditrapani@gmail.com>
2023-02-10 23:56:52 -0500

    feat: define a hello-world express server

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,16 @@
+import express, { Express, Request, Response } from 'express'
+
 export const hello = () => 'Hello world!\n'
 
 console.log(hello())
+
+const app: Express = express()
+const port = 8000
+
+app.get('/', (_req: Request, res: Response) => {
+  res.send('This is plants-n-pests server...what do you want?')
+})
+
+app.listen(port, () => {
+  console.log(`Plants-n-pests server up on ${port}`)
+})
