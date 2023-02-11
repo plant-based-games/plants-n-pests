1b070f5d1ce4c694c49e4a66d9d45ce6bf72f9cf
Lyall Jonathan Di Trapani <lj.ditrapani@gmail.com>
2023-02-11 11:06:48 -0500

    feat: get session cookies working
    The session cookie contains a player that IDs the player (1-4).

---
+++ manual-test/login.sh
@@ -0,0 +1,1 @@
+curl -vv -X POST --cookie-jar manual-test/cookie.txt http://localhost:8000/login
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import express, { Express, Request, Response } from 'express'
+import cookieSession from 'cookie-session'
 import { hello } from './hello.js'
 
 console.log(hello())
@@ -7,10 +8,28 @@ const app: Express = express()
 app.disable('x-powered-by')
 app.use(express.static('public'))
 app.use(express.json())
+app.use(
+  cookieSession({
+    name: 'session',
+    keys: ['changeme'],
+    httpOnly: true,
+    maxAge: 24 * 60 * 60 * 1000, // 24 hours
+  }),
+)
 const port = 8000
 
-app.get('/turn', (_req: Request, res: Response) => {
-  res.send('This is plants-n-pests server...what do you want?')
+app.post('/login', (req: Request, res: Response) => {
+  req.session = { player: 1 }
+  res.send({ player: 1 })
+})
+
+app.post('/play/:cardId', (req: Request, res: Response) => {
+  if (req.session && req.session['player']) {
+    const player = req.session['player']
+    res.send(`processed player ${player} turn`)
+  } else {
+    res.status(401).end()
+  }
 })
 
 app.listen(port, () => {
