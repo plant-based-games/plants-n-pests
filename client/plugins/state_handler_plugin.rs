edeeefb259211005dcec3fca2d908bc511e781ab
Jordan Butterwick <jordan@butterwick.tech>
2023-02-12 00:02:24 -0700

    fix: move asset folder to root directory

--- client/plugins/state_handler_plugin.rs
+++ client/plugins/state_handler_plugin.rs
@@ -100,7 +100,7 @@ mod splash {
                 });
             });
         // Insert the timer as a resource
-        commands.insert_resource(SplashTimer(Timer::from_seconds(1.0, TimerMode::Once)));
+        commands.insert_resource(SplashTimer(Timer::from_seconds(5.0, TimerMode::Once)));
     }
 
     // Tick the timer, and change state when finished
