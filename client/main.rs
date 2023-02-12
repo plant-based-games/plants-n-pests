2919136dadb406e5fa9fbe81345f8336dfd5b5f0
Jordan Butterwick <jordan@butterwick.tech>
2023-02-11 20:16:34 -0700

    feat: browser window handling

--- client/main.rs
+++ client/main.rs
@@ -5,8 +5,22 @@ use crate::plugins::hello_plugin::HelloPlugin;
 use bevy::app::App;
 use bevy::prelude::*;
 
+#[cfg(target_arch = "wasm32")]
+fn handle_browser_resize(mut windows: ResMut<Windows>) {
+    let window = windows.get_primary_mut().unwrap();
+    let wasm_window = web_sys::window().unwrap();
+    let (target_width, target_height) = (
+        wasm_window.inner_width().unwrap().as_f64().unwrap() as f32,
+        wasm_window.inner_height().unwrap().as_f64().unwrap() as f32,
+    );
+    if window.width() != target_width || window.height() != target_height {
+        window.set_resolution(target_width, target_height);
+    }
+}
+
 fn main() {
     App::new()
+
         .add_plugins(DefaultPlugins.set(WindowPlugin {
             window: WindowDescriptor {
                 title: String::from("Plants & Pests"),
@@ -16,6 +30,10 @@ fn main() {
             },
             ..default()
         }))
-        .add_plugin(HelloPlugin)
-        .run();
+        .add_plugin(HelloPlugin);
+
+    #[cfg(target_arch = "wasm32")]
+    app.add_system(handle_browser_resize);
+
+    app.run();
 }
